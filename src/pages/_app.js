23a1fbb400a66c47b36981088ceb7e1ee7733ad9
jnutterdev <hello@jnutterdev.com>
2023-03-19 14:18:36 -0400

    Started setting up pokegame with next.js. Added nes.css and press start 2p font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
+import { Press_Start_2P } from '@next/font/google';
 import '@/styles/globals.css'
+import "nes.css/css/nes.min.css";
+
+export const text = Press_Start_2P({
+  subsets: ['latin'],
+  weight: ['400'],
+  fallback: ['cursive', 'sans-serif'],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <main className={text.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
